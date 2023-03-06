8aba00a0d80fa5dbdd76f94ebb36fc881a8af609
Gulten_Cydeo <g73h28@gmail.com>
2023-03-06 09:32:16 -0500

    created rememberMe feature and Step Definitions

---
+++ src/test/java/com/azulcrm/step_definitions/RememberMe_StepDef.java
@@ -0,0 +1,30 @@
+package com.azulcrm.step_definitions;
+
+import com.azulcrm.pages.LoginPage;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class RememberMe_StepDef {
+   LoginPage loginPage = new LoginPage();
+
+
+    @Then("click the check box")
+    public void click_the_check_box () {
+        loginPage.rememberMeCheckBox.click();
+        Assert.assertTrue(loginPage.rememberMeCheckBox.isSelected());
+
+    }
+    @Then("see Remember me on this computer message displayed")
+    public void verify_there_is_a_remember_me_on_this_computer_message_displayed(String expectedText) {
+        String actualText = loginPage.rememberMeText.getText();
+      //Assert.assertTrue(loginPage.rememberMeText.isDisplayed());
+       Assert.assertEquals(expectedText, actualText);
+
+    }
+
+
+
+
+
+
+}
