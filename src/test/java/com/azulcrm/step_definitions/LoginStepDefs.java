a7b9b8d2864753828850dab4656f2c5a62430432
Sagcatas <114304342+sagcatas@users.noreply.github.com>
2023-03-08 15:51:57 -0500

    US05 and US07 updated according to US stories

--- src/test/java/com/azulcrm/step_definitions/LoginStepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/LoginStepDefs.java
@@ -119,6 +119,74 @@ public class LoginStepDefs {
         Assert.assertEquals(expectedText, actualText);
     }
 
+    @Given("the {string} is on the home page")
+    public void the_userType_on_the_homepage(String userType) {
+        //based on input enter that user information
+        String username = null;
+        String password = null;
 
+        switch (userType.toLowerCase()) {
+            case "hr_user":
+                username = ConfigurationReader.getProperty("hr_user");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username1":
+                username = ConfigurationReader.getProperty("hr_user1");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username2":
+                username = ConfigurationReader.getProperty("hr_user2");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username3":
+                username = ConfigurationReader.getProperty("hr_user3");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username4":
+                username = ConfigurationReader.getProperty("hr_user4");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username5":
+                username = ConfigurationReader.getProperty("hr_user5");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username6":
+                username = ConfigurationReader.getProperty("hr_user6");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username7":
+                username = ConfigurationReader.getProperty("hr_user7");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username8":
+                username = ConfigurationReader.getProperty("hr_user8");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username9":
+                username = ConfigurationReader.getProperty("hr_user9");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username10":
+                username = ConfigurationReader.getProperty("hr_user10");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "hr_username11":
+                username = ConfigurationReader.getProperty("hr_user11");
+                password = ConfigurationReader.getProperty("hr_password");
+                break;
+            case "helpdesk_user":
+                username = ConfigurationReader.getProperty("helpdesk_user");
+                password = ConfigurationReader.getProperty("hd_password");
+                break;
+            case "marketing_user":
+                username = ConfigurationReader.getProperty("marketing_user");
+                password = ConfigurationReader.getProperty("mrkt_password");
+                break;
+
+        }
+
+        //send username and password and login
+        new LoginPage().login(username, password);
+    }
 
 }
