32a4a846df202e827d17a8962421a80bb2b7e03f
KFrankPA <114306682+KFrankPA@users.noreply.github.com>
2023-03-08 18:48:10 -0500

    us04

---
+++ src/test/java/com/azulcrm/step_definitions/US04_DesktopApps.java
@@ -0,0 +1,44 @@
+package com.azulcrm.step_definitions;
+
+import com.azulcrm.pages.Employees;
+import com.azulcrm.pages.LoginPage;
+import com.azulcrm.utilities.ConfigurationReader;
+import com.azulcrm.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class US04_DesktopApps {
+    LoginPage loginPage = new LoginPage();
+    Employees employees = new Employees();
+    @Given("user puts correct password and login to sign in")
+    public void userPutsCorrectPasswordAndLoginToSignIn() {
+        loginPage.login(ConfigurationReader.getProperty("hr_user2"),ConfigurationReader.getProperty("hd_password"));
+    }
+
+    @Then("verify the users sees desktop apps")
+    public void verifyTheUsersSeesDesktopApps(List<String> desktopAppsExpected) {
+
+        List <String> actualValue = getTxt(employees.desktopApps);
+        Assert.assertTrue(actualValue.equals(desktopAppsExpected));
+
+
+    }
+    public static List<String> getTxt(List<WebElement> listOfElements) {
+
+        List<String> txtValue = new ArrayList<>();
+        for (WebElement webElement : listOfElements) {
+            txtValue.add(webElement.getText());
+        }
+        return txtValue;
+    }
+
+    @Given("user goes to login page")
+    public void userGoesToLoginPage() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+    }
+}
