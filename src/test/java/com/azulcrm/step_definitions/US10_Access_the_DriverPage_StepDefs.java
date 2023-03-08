c928bf79e31879116555d6feb65704c367eff3b4
Samira <alieva.samira.as@gmail.com>
2023-03-08 18:27:32 -0500

    US10 please check, updated

--- src/test/java/com/azulcrm/step_definitions/US10_Access_the_DriverPage_StepDefs.java
+++ src/test/java/com/azulcrm/step_definitions/US10_Access_the_DriverPage_StepDefs.java
@@ -19,16 +19,17 @@ import org.openqa.selenium.support.ui.Select;
 import java.util.ArrayList;
 import java.util.List;
 
-public class US10_Access_the_DriverPage_StepDefs extends BasePage {
+public class US10_Access_the_DriverPage_StepDefs {
+    DrivePage drivePage = new DrivePage();
     @Given("users click the Drive module")
     public void users_click_the_drive_module() {
-        driveModule.click();
+        BasePage.driveModule.click();
 
     }
 
     @Then("verify the users see folowing 6 options:")
     public void verify_the_users_see_flowing_options(List<String> expectedOptions) {
-        List<String> actualOptions = BrowserUtils.getElementsText(options_drive_header);
+        List<String> actualOptions = BrowserUtils.getElementsText(BasePage.options_drive_header);
         Assert.assertEquals(expectedOptions, actualOptions);
 
 
